e6e63fdd3673c06537e9ea46e9a5bbac8374396c
gonchuki <gonchuki@gmail.com>
2026-08-08 00:51:14 -0300

    fix(client): schemabadge accessibility nit

--- client/src/components/shared/SchemaBadge.tsx
+++ client/src/components/shared/SchemaBadge.tsx
@@ -15,7 +15,7 @@ export function SchemaBadge({ bgcolor, textcolor, className, children }: PropsWi
         backgroundColor: bgcolor,
         color: textcolor,
       }}
-      aria-hidden={!children}
+      aria-hidden="true"
     >
       {children}
     </span>
